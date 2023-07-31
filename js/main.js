fc2b6c03514af9d8574b872cc60281c24970f25a
Alisher <turdimuratovash@gmail.com>
2023-07-31 14:05:41 +0500

    Js1-dars
    24-gr Shakhsanem

---
+++ js/main.js
@@ -0,0 +1,65 @@
+/*
+console.log(4+4*6)
+alert("Saytimizga xush kelibsiz!")
+document.write("Men body oynasida ko'rinaman!")*/
+/*
+let x=2;
+document.write(x);*/
+/*
+let x=2;
+let y=5;
+x=10;
+*/
+/*
+let x=10, y=5, z=2;
+*/
+/*
+document.write(x+y+z);*/
+/*
+Data type in JS*/
+/*
+let x=5;/!*number*!/
+let matn="Javascript"/!*string*!/
+document.write(typeof matn);*/
+/*Math in JS*/
+/*
+let x=5;
+x+=12/!*x=5+12 *!/
+document.write(x);
+*/
+/*let x=5;*/
+/*x-=3;*/
+/*x*=5;*/
+/*x/=2;*/
+// document.write(x);
+/*let qoldiq=x%9;*/
+/*let daraja=x**3*/
+/*++x;*/
+/*--x;
+--x;
+--x;*/
+/*x++;*/
+
+
+/*
+document.write(x+"5" +
+    "");*/
+/*if(agar)*/
+/*else (u holda)*/
+/*else if (yoki bo'lmasa)*/
+/*
+if ("Salom"=="Salo"){
+    console.log("Ular teng")
+}
+else {
+    console.log("Ular teng emas!")
+}*/
+if (45==44){
+    console.log(" Birinchisidagi Qiymatlar to'g'ri ")
+}
+else if(45==46){
+    console.log( "Ikkinchisidagi qiymatlar teng")
+}
+else{
+    console.log("Ikkalasidagi qiymatlar teng emas")
+}
